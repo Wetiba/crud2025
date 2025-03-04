122e6f61f717a5e7830a6d478c32f7de6cb31a1a
wetiba <erickw006@gmail.com>
2025-03-04 09:59:30 +0300

    updated views and url

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
 path('',views.user_list),
-    path('Add/',views.adduser)
+    path('Add/',views.adduser),
+    path('Edit/<id>',views.Edituser),
+    path('Delete/<eid>',views.Deleteuser),
+    path('View/<eid>',views.Viewuser)
 
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,41 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import User
 from .forms import UserForm
+
 
 # Create your views here.
 def user_list(request):
     users = User.objects.all()
-    return render(request,'listingpage.html',{'users':users})
+    return render(request, 'listingpage.html', {'users': users})
+
+
 def adduser(request):
-    mydict={}
+    mydict = {}
     form = UserForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         form.save()
         return redirect('/')
     mydict['form'] = form
-    return render(request,'add.html',mydict)
+    return render(request, 'add.html', mydict)
+
+
+def Edituser(request, id=None):
+    one_rec = User.objects.get(pk=id)
+    form = UserForm(request.POST or None, request.FILES or None, instance=one_rec)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    mydict = {'form': form}
+    return render(request, 'edit.html', context=mydict)
+def Deleteuser(request, eid=None):
+    one_rec = User.objects.get(pk=eid)
+    if request.method == 'POST':
+        one_rec.delete()
+        return redirect('/')
+    return render(request,'delete.html')
+def Viewuser(request,eid=None):
+    mydict ={}
+    one_rec = User.objects.get(pk=eid)
+    mydict['user'] = one_rec
+    return render(request, 'view.html',mydict)
+
